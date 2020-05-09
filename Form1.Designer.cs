cb087d3d1014bd67a5a8f61e9b35f194265641e2
adnane00-kej <64872898+adnane00-kej@users.noreply.github.com>
2020-05-09 01:25:55 +0000

    bbbb

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -50,7 +50,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(363, 173);
+            this.button1.Location = new System.Drawing.Point(435, 100);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
